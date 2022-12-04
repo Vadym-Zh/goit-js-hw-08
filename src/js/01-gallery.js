331c4bd695c2f176af05f74aa0494b83df03577e
Vadym Zhuravlov <vadiazh@gmail.com>
2022-12-04 14:21:39 +0200

    task1, task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryItemMarkup = creatGalleryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+function creatGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+         <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+       </li>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+// {
+/* <iframe
+  id="vimeo-player"
+  src="https://player.vimeo.com/video/236203659"
+  width="640"
+  height="360"
+  frameborder="0"
+  allowfullscreen
+  allow="autoplay; encrypted-media"
+></iframe>; */
+// }
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(timeSave, 1000));
+
+function timeSave() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+console.log(
+  'localStorage.getItem(videoplayer-current-time)',
+  localStorage.getItem('videoplayer-current-time')
+);
